79af63d0926177449d000d151048f92814465335
DieGoldEnte <54498685+DieGoldEnte@users.noreply.github.com>
2026-04-07 18:25:03 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 1. PFADE DEFINIEREN (Jetzt exakt passend zu deinem Dockerfile!)
+# 1. DEFINING PATHS
 INSTALL_DIR="/ark"
 STEAMCMD_DIR="/steamcmd"
 CONFIG_DIR="$INSTALL_DIR/ShooterGame/Saved/Config/LinuxServer"
 
 mkdir -p "$CONFIG_DIR"
 
-echo "--- Step 1: Generating Configurations (Gamma Fix included) ---"
+echo "--- Step 1: Generating Configurations ---"
 cat <<EOF > "$CONFIG_DIR/GameUserSettings.ini"
 [ServerSettings]
 XPMultiplier=${ARK_XP_MULTIPLIER:-2.0}
@@ -28,10 +28,10 @@ ResourceNoReplenishRadiusPlayers=0.1
 ServerAdminPassword=${ARK_ADMIN_PASSWORD:-CHANGE_ME}
 ServerPassword=${ARK_SERVER_PASSWORD:-CHANGE_ME}
 bGiveDefaultSurvivorItems=False
-# GAMMA FIX
+; GAMMA FIX
 DisablePvEGamma=False
-DisablePvPGamma=False
-AllowGammaVariable=True
+EnablePvPGamma=True
+ServerPVE=${ARK_PVE:-False}
 
 [/Script/ShooterGame.ShooterGameUserSettings]
 MasterAudioVolume=1.000000
@@ -60,14 +60,14 @@ BabyImprintAmountMultiplier=1.0
 bUseCorpseLocator=True
 EOF
 
-echo "--- Step 2: Checking ARK Installation (Smart-Check) ---"
+echo "--- Step 2: Checking ARK Installation ---"
 if [ ! -f "$INSTALL_DIR/ShooterGame/Binaries/Linux/ShooterGameServer" ]; then
     echo "--- ARK Server files not found. Starting Installation... ---"
-    # Da SteamCMD schon im Image ist, rufen wir es hier einfach direkt auf
     $STEAMCMD_DIR/steamcmd.sh +force_install_dir $INSTALL_DIR +login anonymous +app_update 376030 validate +quit
 fi
 
 echo "--- Step 3: Starting ShooterGameServer ---"
 cd "$INSTALL_DIR/ShooterGame/Binaries/Linux" || exit
 
-exec ./ShooterGameServer ${ARK_MAP:-Fjordur}?listen?SessionName="${ARK_SESSION_NAME}"?ServerPassword=${ARK_SERVER_PASSWORD}?ServerAdminPassword=${ARK_ADMIN_PASSWORD}?MaxPlayers=${ARK_MAX_PLAYERS:-10}?XPMultiplier=${ARK_XP_MULTIPLIER:-2.0}?TamingSpeedMultiplier=${ARK_TAMING_MULTIPLIER:-5.0}?HarvestAmountMultiplier=${ARK_HARVEST_MULTIPLIER:-8.0}?DisablePvEGamma=false?DisablePvPGamma=false?AllowGammaVariable=true -server -log -NoBattlEye
+# Gamma Fix
+exec ./ShooterGameServer ${ARK_MAP:-Fjordur}?listen?SessionName="${ARK_SESSION_NAME}"?ServerPassword=${ARK_SERVER_PASSWORD}?ServerAdminPassword=${ARK_ADMIN_PASSWORD}?MaxPlayers=${ARK_MAX_PLAYERS:-10}?DisablePvEGamma=false?EnablePvPGamma=true -server -log -NoBattlEye
